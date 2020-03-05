58ac7128d961488ecd48b98be51357c9eec248be
Don de Dieu BOLENGE <dondedieubolenge@gmail.com>
2020-03-05 12:10:51 +0100

    Ajout des nouvelles fonctions

--- src/helpers.php
+++ src/helpers.php
@@ -17,3 +17,42 @@
             }
         }
     }
+
+    if (!function_exists('is_email_valid')) {
+        /**
+         * Vérifie si l'email passé en paramètre est valide
+         * @param string $email L'adresse email en question
+         * @return bool
+         */
+        function is_email_valid(string $email) {
+            return filter_var($email, FILTER_VALIDATE_EMAIL);
+        }
+    }
+
+    if (!function_exists('make_to_pluriel')) {
+        /**
+         * Permet de renvoyé un 's' si la valeur passé est > 1
+         * @param mixed $value
+         * @return string
+         */
+        function make_to_pluriel($value) {
+            return $value > 1 ? 's' : '';
+        }
+    }
+
+    if (!function_exists('is_int_valid')) {
+        /**
+         * Vérifie si la valeur passée en paramètre est réellement un entier
+         * @param mixed $value La valeur à vérifier
+         * @return boolean
+         */
+        function is_int_valid($value) {
+            $value = (int) $value;
+
+            if ($value > 0) {
+                return true;
+            }
+
+            return false;
+        }
+    }
